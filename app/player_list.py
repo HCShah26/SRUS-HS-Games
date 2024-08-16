786f99fb2f6c1731ae572e945c2b90e4455e60d0
Hiten Shah` <hcshah26@hotmail.com>
2024-08-16 17:01:51 +0800

    Added insert PlayerNode method

--- app/player_list.py
+++ app/player_list.py
@@ -1,4 +1,19 @@
+from app.player_node import PlayerNode
 class PlayerList:
 
     def __init__(self):
         self._head = None
+
+    def is_empty(self) -> bool:
+        """
+        Checks if the first node is null in the PlayerList, returns true if it is empty, false otherwise
+        :return:
+        """
+        return self._head is None
+
+    def insert_node(self, player_node: PlayerNode):
+        if self.is_empty():
+            self._head = player_node
+        else:
+            player_node.next_node = self._head
+            self._head = player_node
---
+++ test/player_list_test.py
@@ -0,0 +1,5 @@
+import unittest
+
+from app.player_list import PlayerList
+
+class TestPlayerList(unittest.TestCase):
